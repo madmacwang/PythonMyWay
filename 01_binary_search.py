e7e35593f3e535982e6c938ff349dca499824db9
Fred Wang <madmacwang@gmail.com>
2019-08-01 22:43:30 +0700

    2019/8/1

--- 01_binary_search.py
+++ 01_binary_search.py
@@ -4,7 +4,7 @@ def binary_search(list, item):
   # low and high keep track of which part of the list you'll search in.
   # low 和 high 用於追蹤你將搜尋列表的哪個部分
   low = 0
-  high = len(list) - 1 # high值等於列表長度減 1，因為 low = 0
+  high = len(list) - 1   # high值等於列表長度減 1，因為 low = 0
 
   # While you haven't narrowed it down to one element ...
   # 只要搜尋範圍沒有縮小到只剩下一個元素
@@ -12,21 +12,21 @@ def binary_search(list, item):
 
     # ... check the middle element
     # ... 搜尋列表中位於 low 和 high 中間的那個元素
-    mid = (low + high) // 2
+    mid = (low + high) // 2   # mid值取整數
     guess = list[mid]
 
     # Found the item.
-    # 如果找到了要搜尋的項目
+    # 如果中間元素等於欲搜尋的項目
     if guess == item:
       return mid
 
     # The guess was too high.
-    # 如果大於欲搜尋的項目
+    # 如果中間元素大於欲搜尋的項目
     if guess > item:
       high = mid - 1
 
     # The guess was too low.
-    # 如果小於欲搜尋的項目
+    # 如果中間元素小於欲搜尋的項目
     else:
       low = mid + 1
 
